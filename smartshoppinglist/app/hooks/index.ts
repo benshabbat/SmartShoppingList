4c08738163cb16499878d03f516911ee2191d8dd
benshabbat <benshabbat27@gmail.com>
2025-08-11 14:59:12 +0300

    refactor: Clean up exports in components, hooks, and utilities for better organization and maintainability

---
+++ smartshoppinglist/app/hooks/index.ts
@@ -0,0 +1,7 @@
+// Custom hooks following clean code principles
+export { useFormField, useFormState } from './useFormState'
+export { useNotification } from './useNotification'
+export { useKeyboardNavigation } from './useKeyboardNavigation'
+
+// Re-export existing hooks
+export { useShoppingList } from './useShoppingList'
---
+++ smartshoppinglist/app/utils/index.ts
@@ -0,0 +1,15 @@
+// Utility exports following clean code principles
+export * from './classNames'
+export * from './validation'
+
+// Re-export existing utilities
+export * from './categories'
+export { 
+  CATEGORY_EMOJIS, 
+  STORAGE_KEYS, 
+  COMMON_PRODUCTS
+} from './constants'
+export * from './helpers'
+export * from './presetLists'
+export * from './smartSuggestions'
+export * from './soundManager'

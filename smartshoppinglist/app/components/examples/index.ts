a42ba4b5c06b0805603d37ba17c9e64d20538d0d
benshabbat <benshabbat27@gmail.com>
2025-08-13 00:23:14 +0300

    feat: Remove SmartShoppingActions and ZeroPropsDrillingDemo components; update index exports for cleaner architecture

--- smartshoppinglist/app/components/examples/index.ts
+++ smartshoppinglist/app/components/examples/index.ts
@@ -3,7 +3,6 @@
  * רכיבים לדוגמה שמציגים את יתרונות הארכיטקטורה ללא props drilling
  */
 
-export { ZeroPropsDrillingDemo } from './ZeroPropsDrillingDemo'
 export { PropsDrillingComparison } from './PropsDrillingComparison'
 export { ShoppingAnalytics } from './ShoppingAnalytics'
-export { SmartShoppingActions } from './SmartShoppingActions'
+
